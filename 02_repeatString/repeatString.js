912a9b485b25557890017a28e8912b864df9fb2e
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2024-12-16 22:06:44 -0500

    I completed the function so it would repeat a given string a given number of time.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let typed = "";
 
-};
+    for (let i = 0; i < num; i++) {
+        typed += string;
+    }
+    
+    return typed;
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
