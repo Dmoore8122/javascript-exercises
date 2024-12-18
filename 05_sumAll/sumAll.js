e1c0671c6d5ea2f40d37c6715b22fe2bda2edc79
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2024-12-18 14:09:33 -0500

    Completed Function.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
 const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        const newNum = num1;
+        num1 = num2;
+        num2 = newNum;
+    }
+    
     let finalSum = 0;
 
     for (i = num1; i <= num2; i++) {
