3e684515bed488b5ce17bdf3d5773f9afa4f2939
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2024-12-17 19:51:57 -0500

    completed function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
 
+    for (i = num1; i <= num2; i++) {
+        finalSum += i; 
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
