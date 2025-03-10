91a0c1dc68cefcd448a16f9c05e9fac1064d6336
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2025-03-10 14:07:13 -0400

    Oldest function is tested and complete.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const ages = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = ages(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = ages(
+          currentPerson.yearOfBirth,
+          currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentPerson : oldest;
+      });
 };
 
 // Do not edit below this line
