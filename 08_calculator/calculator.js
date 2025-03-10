b363a2b4cfd548d24c9b87f9d9f646d9c8cac953
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2025-03-10 12:41:05 -0400

    subtract function is tested and complete.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,12 +2,13 @@ const add = function(a, b) {
 	return a + b ;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let value = arr.reduce((sum, current) => sum + current, 0);
+  return value;
 };
 
 const multiply = function() {
